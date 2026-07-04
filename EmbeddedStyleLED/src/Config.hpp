d922b82db7603a0a2aaf769b5162ab40a45a6b29
Pavlo Davydov <mail@pavlix.me>
2026-07-04 13:18:43 +0300

    Add button with interrupt to embedded style LED

--- EmbeddedStyleLED/src/Config.hpp
+++ EmbeddedStyleLED/src/Config.hpp
@@ -5,5 +5,6 @@ namespace cfg
 {
     inline constexpr uint32_t kBaudRate = 115200;
     inline constexpr uint8_t kLedPin = 17;
+    inline constexpr uint8_t kButtonPin = 16;
     inline constexpr uint32_t kBlinkMs = 500;
 }
--- EmbeddedStyleLED/src/main.cpp
+++ EmbeddedStyleLED/src/main.cpp
@@ -4,19 +4,34 @@
 
 Led led1 = Led(cfg::kLedPin);
 uint32_t lastToggle = 0;
+volatile bool buttonPressed = false;
+bool blinking = true;
+
+void IRAM_ATTR onButtonPress()
+{
+  buttonPressed = true;
+}
 
 void setup()
 {
   Serial.begin(cfg::kBaudRate);
   led1.init();
+  pinMode(cfg::kButtonPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(cfg::kButtonPin), onButtonPress, FALLING);
 }
 
 void loop()
 {
   uint32_t now = millis();
 
-  if (now - lastToggle >= cfg::kBlinkMs){
+  if (blinking && (now - lastToggle >= cfg::kBlinkMs))
+  {
     led1.toggle();
     lastToggle = now;
+  }
+
+  if (buttonPressed){
+    buttonPressed = false;
+    blinking = !blinking;
   }
 }
